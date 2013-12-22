de864e41b530980244e81bfa357f13f3b8b13ac7
Carl <carlos.or.lopez@gmail.com>
2013-12-22 07:03:57 +0100

    Restaurado caca bundler desde mainstation en casa ...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'e9f89bafaff90b50b87e69678097feefc25e4a0196e589394dda9a8ed5c7d3a7a2e422137af7b7a481be3c672ea628026d43d5f009080a0c0c9b3ff0d19b7843'
