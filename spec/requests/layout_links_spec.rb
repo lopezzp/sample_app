76566c8ca4a964e50ea69114fcf009649099c8f3
Carl <carlos.or.lopez@gmail.com>
2014-01-01 10:16:22 +0100

    [Chapter 5 ] Ejercicios finiquitados, crear helper function y hacer que integration test para verificar que los links funcionan ...

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,22 +2,27 @@ require 'spec_helper'
 
 	describe "LayoutLinks" do
 		it "should have a Home page at '/'" do
-		get '/'
+		visit root_path
+		click_link "Home"
 		response.should have_selector('title', :content => "Home")
 	end
 	it "should have a Contact page at '/contact'" do
-		get '/contact'
+		visit root_path
+		click_link "Contact"
 		response.should have_selector('title', :content => "Contact")
 	end
 	it "should have an About page at '/about'" do
-		get '/about'
+		visit root_path
+		click_link about_path
 		response.should have_selector('title', :content => "About")
 	end
 	it "should have a Help page at '/help'" do
-		get '/help'
+		visit root_path
+		click_link help_path
 		response.should have_selector('title', :content => "Help")
 	end
 	it "should have a Sign Up page at '/signup'" do 
-		get '/new'
+		visit root_path
+		click_link signup_path
 		response.should have_selector("title",:content => "Sign up")
 end
